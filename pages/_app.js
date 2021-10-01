fc7300001219c29e5471f35a7dbfee9087ba4879
Nathalie Baladejo-Reynosa <nathy.reynosa@protonmail.com>
2021-10-01 05:01:38 +0000

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../styles/globals.css'
 import initAuth from '../utils/initAuth'
+import { ChakraProvider } from "@chakra-ui/react" // <- add this
 
 initAuth()
 
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider> 
+      <Component {...pageProps} />
+    </ChakraProvider> // <- add this
+  )
 }
 
 export default MyApp
