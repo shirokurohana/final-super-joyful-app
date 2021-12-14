b8d49bfa2124b49a57f2c24820e931a2343230a6
Nathalie Baladejo-Reynosa <nathy.reynosa@protonmail.com>
2021-12-13 20:03:49 -0800

    Psalm 51

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
+// 12/13/2021: All thanks to Mika Di Lello from the CS 55.13 class
+
 const config = {
   webpack5: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false,
+            http: false,
+            stream: false,
+            https: false,
+            net: 'empty'
+        }
+    }
+
+    return config;
+}
 }
 
 // eslint-disable-next-line import/no-extraneous-dependencies
